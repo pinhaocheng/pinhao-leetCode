f2fbb04d8deada73bc5c06d6cec1c318e94dd7cf
Pin-Hao Cheng <cpinhao98@gmail.com>
2025-07-16 21:49:21 -0500

    day 2 of 75

---
+++ 1071. Greatest Common Divisor of Strings/Greatest common divisor of strings.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+        {
+            return "";
+        }
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int gcd_length = std::gcd(len1, len2);
+        return str1.substr(0, gcd_length);
+    }
+};
