3c4d496c9524fe4f4fd2acccbe0fa55f51aae932
Pin-Hao Cheng <cpinhao98@gmail.com>
2025-07-22 22:20:03 -0500

    Refactor productExceptSelf to eliminate unnecessary arrays and reduce space complexity

--- 238. Product of Array Except Self/Product of Array Except Self.cpp
+++ 238. Product of Array Except Self/Product of Array Except Self.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        std::vector<int> left_products(nums.size(), 0);
-        std::vector<int> right_products(nums.size(), 0);
         std::vector<int> answer(nums.size(), 0);
+        int right = 1;
 
-        left_products[0] = 1;
-        right_products[nums.size() - 1] = 1;
+        answer[0] = 1;
 
         for(int i = 1; i < nums.size(); ++i){
-            left_products[i] = left_products[i - 1] * nums[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1];
         }
 
-        for(int i = nums.size() - 2; i >= 0; --i){
-            right_products[i] = right_products[i + 1] * nums[i + 1];
-        }
-
-        for (int i = 0; i < nums.size(); ++i) {
-            answer[i] = left_products[i] * right_products[i];
+        for(int i = nums.size() - 1; i >= 0; --i){
+            answer[i] *= right;
+            right *= nums[i];
         }
 
         return answer;
