f8a6f527a05ddb3b4c685b90e2f9d37b8fe010f2
Pin-Hao Cheng <cpinhao98@gmail.com>
2025-07-29 23:46:18 -0500

    Refactor maxOperations function in "Max Number of K-Sum Pairs" to use two-pointer technique for improved efficiency

--- 1679. Max Number of K-Sum Pairs/Max Number of K-Sum Pairs.cpp
+++ 1679. Max Number of K-Sum Pairs/Max Number of K-Sum Pairs.cpp
@@ -6,35 +6,34 @@ class Solution
 public:
     int maxOperations(std::vector<int> &nums, int k)
     {
-        // 1. Initialize an unordered_map and an operations counter.
-        // Declares a map named 'frequencies' where keys are integers // and values are also integers.
-        std::unordered_map<int, int> frequencies;
+        // Sort the entire vector in ascending order
+        std::sort(nums.begin(), nums.end());
+
+        // Initialize a counter for the number of operations
         int success = 0;
 
-        // 2. Loop through the nums vector.
-        for (int num : nums)
+        // Initialize two pointers, one at the start and one at the end of the vector
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right)
         {
-            int complement = k - num;
-            // First, check if the key 'complement' exists in the map
-            // AND if its count is more than 0.
-            // Apparently this is called a short circuit
-            // It's a way to check if the key exists and if it does, it's greater than 0
-            // If it doesn't exist, it doesn't check the second condition
-            if (frequencies.count(complement) && frequencies[complement] > 0)
+            if (nums[left] + nums[right] == k)
             {
-                // If it does, we found a pair!
-                // Decrement the count of the complement since we've "used" it.
-                frequencies[complement]--;
                 success++;
+                left++;
+                right--;
+            }
+            else if (nums[left] + nums[right] < k)
+            {
+                left++;
             }
             else
             {
-                // This current `num` did not find a partner.
-                // So, we add it to the map to be a partner for a future number.
-                frequencies[num]++;
+                right--;
             }
         }
-        // 3. Return the counter.
+
         return success;
     }
 };
